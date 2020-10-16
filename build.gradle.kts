c58b66f5c2ad4da51138c2ae3f78ce3a249ef592
Per Jørgen Walstrøm <per.jorgen.walstrom@nav.no>
2020-10-16 16:59:07 +0200

    bump kotless 0.1.6->0.1.7-beta-4, gradle 6.3->6.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,18 +4,17 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.3.72" apply true
-    id("io.kotless") version "0.1.6" apply true
+    id("io.kotless") version "0.1.7-beta-4" apply true
 }
 group = "me.pj"
 version = "1.0-SNAPSHOT"
 
 repositories {
     jcenter()
-    mavenCentral()
 }
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("io.kotless", "kotless-lang", "0.1.6")
+    implementation("io.kotless", "kotless-lang", "0.1.7-beta-4")
 }
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
@@ -26,17 +25,17 @@ kotless {
         bucket = "www.waperon.org"
 
         terraform {
-            profile = "pj.kotless"
+            profile = "pj"
             region = "eu-north-1"
         }
     }
 
     webapp {
-        lambda {
-            kotless {
-                packages = setOf("me.pj")
-            }
-        }
+//        lambda {
+//            kotless {
+//                packages = setOf("me.pj")
+//            }
+//        }
         //route53 alias for deployed application, must be in us-east-1?
         route53 = Route53("_e491cf4eb0d63b4d22903af02782d761", "waperon.org")
     }
